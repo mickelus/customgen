7dc3467b512f18a7739d65ef5cfea3d71db87650
ser0n <andre.lilja@gmail.com>
2013-08-03 19:13:37 +0200

    updated constants

--- common/se/mickelus/modjam/Constants.java
+++ common/se/mickelus/modjam/Constants.java
@@ -6,7 +6,8 @@ public class Constants {
 	public static final String MOD_NAME = "modjam - layernine";
 	public static final String VERSION = "0.1";
 	
-	public static final String SAVE_PATH = "/saves/modjam.dat";
+	public static final String SAVE_PATH = "resourcepacks/assets/modjam/";
+	public static final String SAVE_NAME = "segments";
 	
 	public static final String SAVE_CMD = "save";
 	public static final String CREATE_BLOCK_CMD = "cb";

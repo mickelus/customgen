c6eb9035f08daa5c9c593f77972ca16ac2ced287
ser0n <andre.lilja@gmail.com>
2013-08-04 17:55:43 +0200

    add .dat to segments file

--- common/se/mickelus/modjam/Constants.java
+++ common/se/mickelus/modjam/Constants.java
@@ -7,12 +7,13 @@ public class Constants {
 	public static final String VERSION = "0.1";
 	
 	public static final String SAVE_PATH = "resourcepacks/assets/modjam/";
-	public static final String SAVE_NAME = "segments";
+	public static final String SAVE_NAME = "segments.dat";
 	
 	public static final String SAVE_CMD = "save";
 	public static final String CREATE_BLOCK_CMD = "cb";
 	public static final String LOAD_CMD = "load";
 	
+	public static final int ROOM_SIZE = 8;
 	public static final int DUNGEON_CHANCE = 100;
 	
 	

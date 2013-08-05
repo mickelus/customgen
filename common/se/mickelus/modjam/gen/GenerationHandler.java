dd5462aa80bca073fff88c1e534a333500677cb4
mickelus <wikner.mikael@gmail.com>
2013-08-06 00:18:14 +0200

    fiddled with generation, changed blocks ids to ints

--- common/se/mickelus/modjam/gen/GenerationHandler.java
+++ common/se/mickelus/modjam/gen/GenerationHandler.java
@@ -1,6 +1,8 @@
 package se.mickelus.modjam.gen;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 import se.mickelus.modjam.Constants;
@@ -15,12 +17,14 @@ import cpw.mods.fml.common.registry.GameRegistry;
 
 public class GenerationHandler implements IWorldGenerator {
 	
-	ArrayList<SegmentPlaceholder> pendingSegments;
+	List<SegmentPlaceholder> pendingSegments;
+	List<int[]> occupiedPositions;
 	
 	
 	public GenerationHandler() {
 		
 		pendingSegments = new ArrayList<SegmentPlaceholder>();
+		occupiedPositions = new LinkedList<int[]>();
 		GameRegistry.registerWorldGenerator(this);
 	}
 
@@ -36,10 +40,9 @@ public class GenerationHandler implements IWorldGenerator {
 			
 			// randomly select a height
 			int y = random.nextInt(3);
-			System.out.println("GENERATED START AT :" + chunkX + ", " + y + ", " + chunkZ);
 			
 			// add placeholder to pending-list
-			pendingSegments.add(new SegmentPlaceholder(chunkX, y, chunkZ, 0, 0, 0, 0, 0, 0, Segment.TYPE_START));	
+			pendingSegments.add(new SegmentPlaceholder(chunkX, y, chunkZ, 0, 0, 0, 0, 0, 0, Segment.TYPE_START));
 		}
 		
 		
@@ -47,55 +50,55 @@ public class GenerationHandler implements IWorldGenerator {
 		do {
 			pendingGeneration = false;
 			// iterate over all placeholders
-			for (SegmentPlaceholder ph : pendingSegments) {
+			for (int i = 0; i<pendingSegments.size(); i++) {
 				// if placeholder is in generated chunk
-				if(chunkProvider.chunkExists(ph.getX(), ph.getY())) {
-					pendingGeneration = true;
+				if(chunkProvider.chunkExists(pendingSegments.get(i).getX(), pendingSegments.get(i).getZ())) {
+					SegmentPlaceholder ph = pendingSegments.get(i);
+					
+					pendingSegments.remove(ph);
+					i--;
+					
 					// generate segment based on placeholder
-					generateSegment(ph, world);
+					generateSegment(ph, world, random);
+					
+					
+					pendingGeneration = true;
 				}
 			}
 				
-		} while(pendingGeneration);	
-			
-		
-		
-		boolean generated = false;
-		// is segment generation pending in this chunk?
-		for (SegmentPlaceholder	segment : pendingSegments) {
-			if(segment.getX() == chunkX && segment.getZ() == chunkZ) {
-				generateSegment(segment, world);
-				generated = true;
-			}
-		}
-		
-		
-		
+		} while(pendingGeneration);
+
 	}
 	
 	
-	private void generateSegment(SegmentPlaceholder placeholder, World world) {
+	private void generateSegment(SegmentPlaceholder placeholder, World world, Random random) {
 		// ask the segmentstore for a segment matching the placeholder
-		Segment segment = SegmentStore.getSegment(placeholder);
+		Segment segment = SegmentStore.getSegment(placeholder, random);
 		
 		// get segment position
 		int x = placeholder.getX() * 16;
 		int y = placeholder.getY() * 16;
 		int z = placeholder.getZ() * 16;
 		
-		
 		// TODO : we should not have to handle this
 		if(segment == null) {
 			System.out.println("Unable to generate segment");
 			return;
 		}
-		System.out.println("GENERATING SEGMENT AT :" + x + ", " + y + ", " + z);
 		
+		System.out.println(String.format("new seg x:%d y:%d z:%d %b",
+				x, y, z,
+				isOccupied(placeholder.getX(), placeholder.getY(), placeholder.getZ())));
+
 		// generate blocks
-		for(int sx = 0; sx < 16; sx++) {
-			for(int sy = 0; sy < 16; sy++) {
-				for(int sz = 0; sz < 16; sz++) {
-					world.setBlock(x+sx, y+sy+4, z+sz, segment.getBlockID(sx, sy, sz));
+		for(int sy = 0; sy < 16; sy++) {
+			for(int sz = 0; sz < 16; sz++) {
+				for(int sx = 0; sx < 16; sx++) {
+					int blockID = segment.getBlockID(sx, sy, sz);
+					if(blockID != -1) {
+						world.setBlock(x+sx, y+sy+4, z+sz, blockID, segment.getBlockData(sx, sy, sz), 2);
+					}
+					
 				}
 			}
 		}
@@ -105,9 +108,16 @@ public class GenerationHandler implements IWorldGenerator {
 		
 		// spawn entities
 		
+
+		
+		// mark this position as occupied
+		occupyPosition(placeholder.getX(), placeholder.getY(), placeholder.getZ());
+		
 		
 		// updated pending segments based on this segments interfaces
-		updatePendingSegments(placeholder);
+		updatePendingSegments(placeholder.getX(), placeholder.getY(), placeholder.getZ(), segment);
+		
+		
 		
 		
 	}
@@ -132,28 +142,42 @@ public class GenerationHandler implements IWorldGenerator {
 	}
 	
 	private void updatePendingSegments(int x, int y, int z, int top, int bottom, int north, int south, int east, int west) {
-		getPlaceholder(x, y+1, z).setInterfaceBottom(top);
-		getPlaceholder(x, y-1, z).setInterfaceTop(bottom);
-		getPlaceholder(x, y, z-1).setInterfaceSouth(north);
-		getPlaceholder(x, y+1, z+1).setInterfaceNorth(south);
-		getPlaceholder(x+1, y+1, z).setInterfaceWest(east);
-		getPlaceholder(x-1, y+1, z).setInterfaceEast(west);
+				
+		if(top != 0 && y<3 && !isOccupied(x, y+1, z))
+			getPlaceholder(x, y+1, z).setInterfaceBottom(top);
+		
+		if(bottom != 0 && y>0 && !isOccupied(x, y-1, z))
+			getPlaceholder(x, y-1, z).setInterfaceTop(bottom);
+		
+		if(north != 0 && !isOccupied(x, y, z-1))
+			getPlaceholder(x, y, z-1).setInterfaceSouth(north);
+		
+		if(south != 0 && !isOccupied(x, y, z+1))
+			getPlaceholder(x, y, z+1).setInterfaceNorth(south);
+		
+		if(east != 0 && !isOccupied(x+1, y, z))
+			getPlaceholder(x+1, y, z).setInterfaceWest(east);
+		
+		if(west != 0 && !isOccupied(x-1, y, z))
+			getPlaceholder(x-1, y, z).setInterfaceEast(west);
 	}
 	
-	private void updatePendingSegments(SegmentPlaceholder ph) {
-		updatePendingSegments(ph.getX(), ph.getY(), ph.getZ(), 
-				ph.getInterfaceTop(), ph.getInterfaceBottom(), 
-				ph.getInterfaceNorth(), ph.getInterfaceSouth(),
-				ph.getInterfaceEast(), ph.getInterfaceWest());
+	private void updatePendingSegments(int x, int y, int z, Segment segment) {
+		updatePendingSegments(x, y, z, 
+				segment.getInterfaceTop(), segment.getInterfaceBottom(), 
+				segment.getInterfaceNorth(), segment.getInterfaceSouth(),
+				segment.getInterfaceEast(), segment.getInterfaceWest());
 	}
 	
-	private SegmentPlaceholder getPlaceholder(int x, int y, int z) {		
+	
+	private SegmentPlaceholder getPlaceholder(int x, int y, int z) {
 		for (SegmentPlaceholder ph : pendingSegments) {
-			if(ph.getX() == x && ph.getY() == y && ph.getX() == z) {
+			if(ph.getX() == x && ph.getY() == y && ph.getZ() == z) {
 				return ph;
 			}
 		}
 		
+		//System.out.println(String.format("new ph  %d %d %d %b", x, y, z, isOccupied(x, y, z)));
 		SegmentPlaceholder ph = new SegmentPlaceholder(x, y, z, 0, 0, 0, 0, 0, 0, 0);
 		pendingSegments.add(ph);
 		
@@ -161,6 +185,22 @@ public class GenerationHandler implements IWorldGenerator {
 	}
 
 	
+	private boolean isOccupied(int x, int y, int z) {
+		for (int[] pos : occupiedPositions) {
+			if(pos[0] == x && pos[1] == y && pos[2] == z) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private void occupyPosition(int x, int y, int z) {
+		occupiedPositions.add(new int[] {x, y, z});
+	}
+	
+	private void clearOccupied() {
+		occupiedPositions.clear();
+	}
 	
 	
 	

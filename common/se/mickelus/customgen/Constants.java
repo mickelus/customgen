c120746e7e05bde8f28c06dbc4615c919c22666d
mickelus <wikner.mikael@gmail.com>
2014-05-11 22:38:42 +0200

    refactored strings

--- common/se/mickelus/customgen/Constants.java
+++ common/se/mickelus/customgen/Constants.java
@@ -2,13 +2,13 @@ package se.mickelus.customgen;
 
 public class Constants {
 	
-	public static final String MOD_ID = "cuge";
-	public static final String MOD_NAME = "customgen";
+	public static final String MOD_ID = "customgen";
+	public static final String MOD_NAME = "Customgen";
 	public static final String VERSION = "0.1";
 	
 	public static final String PACKS_PATH = "resourcepacks";
-	public static final String GENS_PATH = PACKS_PATH + "/%s/assets/" + MOD_NAME;
-	public static final String GENS_REL_PATH = "assets/" + MOD_NAME;
+	public static final String GENS_PATH = PACKS_PATH + "/%s/assets/" + MOD_ID;
+	public static final String GENS_REL_PATH = "assets/" + MOD_ID;
 	public static final String FILE_PATH = GENS_PATH + "/%s";
 	public static final String FILE_EXT = ".nbt";
 	
@@ -16,30 +16,25 @@ public class Constants {
 	public static final int DUNGEON_CHANCE = 100;
 	public static final int SEGMENT_LIMIT = 50;
 	
+	public static final int GENERATION_WEIGHT = 10;
+	
 	public static final String CHANNEL = MOD_ID;
 	
-	public static final String TEXTURE_LOCATION = MOD_NAME;
+	
+	public static final String TEXTURE_LOCATION = MOD_ID;
 	public static final String BOOKGUI_TEXTURE = "textures/gui/book.png";
 	
-	public static int EMPTY_ID = 2000;
 	public static final String EMPTY_TEXTURE = "empty";
-	public static final String EMPTY_UNLOC_NAME = "EmptyBlock";
-	public static final String EMPTY_NAME = "Empty Block";
+	public static final String EMPTY_UNLOC_NAME = "emptyblock";
 	
-	public static int INTERFACEBLOCK_ID = 2001;
 	public static final String INTERFACEBLOCK_TEXTURE = "interface";
-	public static final String INTERFACEBLOCK_UNLOC_NAME = "InterfaceBlock";
-	public static final String INTERFACEBLOCK_NAME = "Interface Block";
+	public static final String INTERFACEBLOCK_UNLOC_NAME = "interfaceblock";
 	
-	public static int PLACEHOLDERITEM_ID = 4000;
 	public static final String PLACEHOLDERITEM_TEXTURE = "phitem";
-	public static final String PLACEHOLDERITEM_UNLOC_NAME = "PlaceholderItem";
-	public static final String PLACEHOLDERITEM_NAME = "Loot placeholder";
+	public static final String PLACEHOLDERITEM_UNLOC_NAME = "placeholderitem";
 	
-	public static int BOOKITEM_ID = 4001;
 	public static final String BOOKITEM_TEXTURE = "book";
-	public static final String BOOKITEM_UNLOC_NAME = "CustomGenerationBook";
-	public static final String BOOKITEM_NAME = "Book of Custom Generation";	
+	public static final String BOOKITEM_UNLOC_NAME = "customgenerationbook";
 	
 	public static final String TUTORIAL_INTRO = 
 			  "This is a tutorial on how to create new terrain generation features "
